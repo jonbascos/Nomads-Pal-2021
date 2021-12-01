c0aec4ac7e88af65d0ee9e1007820317fc41c290
Jon <jon@Jons-MacBook-Pro.local>
2021-12-01 15:45:27 -0800

    Added 'consider suggesting' message to Hero

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import '../styles/App.css'
 import '../styles/HeroSection.css'
 
@@ -8,6 +9,7 @@ function HeroSection() {
            {/* <video src="/videos/video-2.mp4" autoPlay loop muted></video> */}
            <h1>A NEW LOCATION AWAITS</h1> 
            <p>Check WiFi Speeds at Locations Throughout Portland, Oregon</p>
+           <p>100% Crowdsourced! Consider <Link to='/contact'>suggesting</Link> your favorite locations.</p>
         </div>
     )
 }
