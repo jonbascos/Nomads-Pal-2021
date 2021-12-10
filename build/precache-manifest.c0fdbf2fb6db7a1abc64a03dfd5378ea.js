62259b8d1140d94e43de7e573139861cc80eb4ce
Jon <jon@Jons-MacBook-Pro.local>
2021-12-10 15:42:52 -0800

    changed location cards to bootstrap cards with images

---
+++ build/precache-manifest.c0fdbf2fb6db7a1abc64a03dfd5378ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4de936934ddd7cf8c9f9bd00228b87f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "341d3ee7bc1fa4df80da",
+    "url": "/static/css/main.81da7e5e.chunk.css"
+  },
+  {
+    "revision": "6a7cfe633634fba7e592",
+    "url": "/static/js/2.40e70754.chunk.js"
+  },
+  {
+    "revision": "6577024cf57ea84e8719ecca8ee4b08e",
+    "url": "/static/js/2.40e70754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "341d3ee7bc1fa4df80da",
+    "url": "/static/js/main.dc08525e.chunk.js"
+  },
+  {
+    "revision": "c84e70a30383060fb5cf",
+    "url": "/static/js/runtime-main.67f991ca.js"
+  }
+]);
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,108 +1,143 @@
-import React,{useEffect, useState} from 'react'
-import {Link} from 'react-router-dom'
-import { onSnapshot, collection } from 'firebase/firestore'
-import CardItem from './CardItem'
-import db from '../static/scripts/firebase'
-
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { onSnapshot, collection } from "firebase/firestore";
+import { Card } from "react-bootstrap";
+import CardItem from "./CardItem";
+import db from "../static/scripts/firebase";
 
 // Styles
-import '../styles/Cards.css'
+import "../styles/Cards.css";
 
 function Cards() {
-    const [locations, setLocations] = useState([])  
-    const [specificCity, setSpecificCity] = useState('All Around Portland')
-    const [specificLocations, setSpecificLocations] = useState([])
-    const cities = []
+  const [locations, setLocations] = useState([]);
+  const [specificCity, setSpecificCity] = useState("All Around Portland");
+  const [specificLocations, setSpecificLocations] = useState([]);
+  const cities = [];
 
-    useEffect(() => 
-        onSnapshot(collection(db, 'Locations'), snapshot => {
-            setLocations(snapshot.docs.map(doc => ({...doc.data(), id: doc.id})))
-        })
-    , [])
+  useEffect(
+    () =>
+      onSnapshot(collection(db, "Locations"), (snapshot) => {
+        setLocations(
+          snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+        );
+      }),
+    []
+  );
 
-    //Creates an array of the different cities
-    const singleCities = locations.map(location => {
-            if(!cities.includes(location.city)) {
-                cities.push(location.city)   
-        }
-    })
-
-    useEffect(() => singleCities, [singleCities])
-
-    const listCities = cities.map(city => {
-        return <option value={city}>{city}</option>
-    })
-
-    const handleChange = e => {
-        setSpecificCity(e.target.value)
+  //Creates an array of the different cities
+  const singleCities = locations.map((location) => {
+    if (!cities.includes(location.city)) {
+      cities.push(location.city);
     }
+  });
 
-    // Filter the cards by the city
-    useEffect(() => {
-        const result = locations.filter(location => location.city === specificCity)
-        setSpecificLocations(result)
-    }, [specificCity, locations])
+  useEffect(() => singleCities, [singleCities]);
 
-    const singleCard = locations.map(location => {
-        return(
-            <div key={location.id}>
-                <li>
-                    <CardItem
-                        location={location.name}
-                        address={location.address}
-                        city={location.city}
-                        state={location.state}
-                        zipcode={location.zipcode}
-                        download={location.download}
-                        upload={location.upload}
-                    />
-                </li>       
-            </div>
-                                  
-        )
-    })
+  const listCities = cities.map((city) => {
+    return <option value={city}>{city}</option>;
+  });
 
-    const specificCard = specificLocations.map(specificLocation => {
-        return(
-            <div key={specificLocation.id}>
-                <li>
-                    <CardItem
-                        location={specificLocation.name}
-                        address={specificLocation.address}
-                        city={specificLocation.city}
-                        state={specificLocation.state}
-                        zipcode={specificLocation.zipcode}
-                        download={specificLocation.download}
-                        upload={specificLocation.upload}
-                    />
-                </li>     
-            </div>
-                            
-        )
-    })
-   
-       return (
-            <div className='cards'>
-                <h1>
-                    Check Out These Locations:
-                    <br />
-                    <select className='city-drop-down' onChange={handleChange}>
-                        <option selected value='All Around Portland'>All Around Portland</option>
-                        {listCities}
-                    </select>
-                </h1>
-                <div className="cards-container">
-                    <div className="cards-wrapper">
-                        <ul className="cards-location">
-                            {specificCity === 'All Around Portland' ? <>{singleCard}</> : <>{specificCard}</>}
-                        </ul>
-                    </div>
-                </div>
-                <p className='add-location-container'>
-                    <button><Link to='/contact'>Suggest a New Location</Link></button>
-                </p>
-            </div>
-    )
+  const handleChange = (e) => {
+    setSpecificCity(e.target.value);
+  };
+
+  // Filter the cards by the city
+  useEffect(() => {
+    const result = locations.filter(
+      (location) => location.city === specificCity
+    );
+    setSpecificLocations(result);
+  }, [specificCity, locations]);
+
+  const singleCard = locations.map((location) => {
+    return (
+      <div>
+        <li>
+          <Card className="bootstrap-card" key={location.id}>
+            <Card.Img variant="top" src="https://via.placeholder.com/286x200" />
+            <Card.Body>
+              <Card.Title className="bootstrap-title">
+                <h2>{location.name}</h2>
+              </Card.Title>
+              <Card.Text>
+                {location.address}
+                <br />
+                {location.city}, {location.state} {location.zipcode}
+              </Card.Text>
+              <Card.Footer>
+                Download (Mbps): {location.download}
+                <br />
+                Upload (Mbps): {location.upload}
+              </Card.Footer>
+            </Card.Body>
+          </Card>
+        </li>
+      </div>
+    );
+  });
+
+  const specificCard = specificLocations.map((specificLocation) => {
+    return (
+      <div>
+        <li>
+          <Card className="bootstrap-card" key={specificLocation.id}>
+            <Card.Img
+              className="bootstrap-image"
+              variant="top"
+              src="https://via.placeholder.com/286x200"
+            />
+            <Card.Body>
+              <Card.Title>
+                <h2>{specificLocation.name}</h2>
+              </Card.Title>
+              <Card.Text>
+                {specificLocation.address}
+                <br />
+                {specificLocation.city}, {specificLocation.state}{" "}
+                {specificLocation.zipcode}
+              </Card.Text>
+              <Card.Footer>
+                Download (Mbps): {specificLocation.download}
+                <br />
+                Upload (Mbps): {specificLocation.upload}
+              </Card.Footer>
+            </Card.Body>
+          </Card>
+        </li>
+      </div>
+    );
+  });
+
+  return (
+    <div className="cards">
+      <h1>
+        Check Out These Locations:
+        <br />
+        <select className="city-drop-down" onChange={handleChange}>
+          <option selected value="All Around Portland">
+            All Around Portland
+          </option>
+          {listCities}
+        </select>
+      </h1>
+      <div className="cards-container">
+        <div className="cards-wrapper">
+          <ul className="cards-location">
+            {specificCity === "All Around Portland" ? (
+              <>{singleCard}</>
+            ) : (
+              <>{specificCard}</>
+            )}
+          </ul>
+        </div>
+      </div>
+      <p className="add-location-container">
+        <button>
+          <Link to="/contact">Suggest a New Location</Link>
+        </button>
+      </p>
+    </div>
+  );
 }
 
-export default Cards
+export default Cards;
